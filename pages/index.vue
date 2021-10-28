6349ebea679b00a1b6a5925777fcf1cbabb6320e
owen <91567654+sussy-balls-impostor@users.noreply.github.com>
2021-10-28 23:44:40 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h1>bing chilling</h1>
     <a href="https://cdn.discordapp.com/attachments/707551577740804136/903394526511657000/maxresdefault.png">bing chilling</a>
     <p>
-      there is a screwdriver in my asshole
+      the chinese government is coming to kill me
     </p>
     <p v-if="rendering === 'server'">
       please send help
@@ -12,6 +12,7 @@
     <ul>
       <li>oh no</li>
       <li>i am going to die</li>
+      <li>mika is cringe</li>
     </ul>
 
     <NuxtLink to="/about">this goes nowhere</NuxtLink>
