6afe129951201a1c86c4be3ee111b6b678c30184
owen <91567654+sussy-balls-impostor@users.noreply.github.com>
2021-10-29 17:25:31 +0200

    Create test.vue

---
+++ pages/test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>bing chilling</h1>
+    <img src="~/static/bingchilling.png" />
+    <p>
+      the chinese government is coming to kill me
+    </p>
+    <p v-if="rendering === 'server'">
+      please send help
+    </p>
+    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
+    <ul>
+      <li>oh no</li>
+      <li>i am going to die</li>
+      <li>mika is cringe</li>
+    </ul>
+
+    <NuxtLink to="/about">this goes nowhere</NuxtLink>
+  </div>
+</template>
+<script>
+export default {
+  asyncData() {
+    return {
+      rendering: process.server ? 'server' : 'client'
+    }
+  }
+}
+</script>
