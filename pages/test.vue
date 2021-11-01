b157d0e7fc25a56d4bafd45455ae25796f941290
owen <91567654+sussy-balls-impostor@users.noreply.github.com>
2021-11-01 20:21:49 +0100

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,19 +1,7 @@
 <template>
   <div>
-    <h1>bing chilling</h1>
+    <h1 href="/tacdrip.png">drip</h1>
     <img src="~/static/bingchilling.png" />
-    <p>
-      the chinese government is coming to kill me
-    </p>
-    <p v-if="rendering === 'server'">
-      please send help
-    </p>
-    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
-    <ul>
-      <li>oh no</li>
-      <li>i am going to die</li>
-      <li>mika is cringe</li>
-    </ul>
 
     <NuxtLink to="/about">this goes nowhere</NuxtLink>
   </div>
@@ -27,3 +15,19 @@ export default {
   }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+h1 {
+  font-family: 'Open Sans', sans-serif;
+}
+p {
+  font-family: 'Open Sans', sans-serif;
+}
+li {
+  font-family: 'Open Sans', sans-serif;
+}
+NuxtLink {
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
