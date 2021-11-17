b25cea027426fa2fe87b998fd519ef046f5a839a
owen <91567654+sussy-balls-impostor@users.noreply.github.com>
2021-11-17 19:25:59 +0100

    Update virtuosentum.vue

--- pages/virtuosentum.vue
+++ pages/virtuosentum.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>ATTENTION CITIZEN!</h1>
-    <img src="~/static/pagacingeii.png" />
+    <img src="~/static/pagacringii.png" />
     <p> This is the Central Intelligentsia of the Chinese Communist Party. 
     您的 Internet 浏览器历史记录和活动引起了我们的注意 YOUR INTERNET ACTIVITY HAS ATTRACTED OUR ATTENTION. 志們注意了 you have been found protesting in the classroom!!!!! 
     這是通知你，你必須 我們將接管台灣 serious crime 以及世界其他地方 100 social credits have been deducted from your account 這對我們所有未來的下屬來說都是一個重要的機會 stop the protest immediately 立即加入我們的宣傳活動，提前獲得 do not do this again! 不要再这样做! 
