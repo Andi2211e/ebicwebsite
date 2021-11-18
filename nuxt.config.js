59a32c36c6e2684c5e7d19492e1e962d6280512a
Andi2211e <94647403+Andi2211e@users.noreply.github.com>
2021-11-18 22:14:58 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global App headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'suspekt',
+    title: 'migrant',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
